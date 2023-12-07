28f53eab7d70175248c1d1423d1d037deb166275
Thiago França da Silva <tfsthiagobr98@outlook.com>
2023-12-07 15:02:28 -0300

    feat: add tenant mode

--- config/app.php
+++ config/app.php
@@ -171,6 +171,7 @@ return [
         App\Providers\HorizonServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
+        // App\Providers\TenancyServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
     ])->toArray(),
 
